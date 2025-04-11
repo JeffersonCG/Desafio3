10979bac266a9b0bc9ca97dd3d14fc050044bba3
JeffersonCG <jeffersonc_ag@hotmail.com>
2025-04-11 13:26:51 -0300

    Initial commit

---
+++ Heroi.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desafio3
+{
+    internal class Heroi
+    {
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+        public string Tipo { get; set; }
+
+        public Heroi(string nome, int idade, string tipo)
+        {
+            Nome = nome;
+            Idade = idade;
+            Tipo = tipo;
+        }
+
+        public void Atacar()
+        {
+            string ataque;
+
+            switch (Tipo.ToLower())
+            {
+                case "mago":
+                    ataque = "usou magia";
+                    break;
+                case "guerreiro":
+                    ataque = "usou espada";
+                    break;
+                case "monge":
+                    ataque = "usou artes marciais";
+                    break;
+                case "ninja":
+                    ataque = "usou shuriken";
+                    break;
+                default:
+                    ataque = "fez um ataque desconhecido";
+                    break;
+            }
+
+            Console.WriteLine($"O {Tipo} atacou usando {ataque}");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Desafio3;
+
+class Program
+{
+    static void Main()
+    {
+        var herois = new List<Heroi>
+        {
+            new Heroi("Gandalf", 150, "mago"),
+            new Heroi("Aragorn", 87, "guerreiro"),
+            new Heroi("Lee", 40, "monge"),
+            new Heroi("Hanzo", 29, "ninja")
+        };
+
+        foreach (var heroi in herois)
+        {
+            heroi.Atacar();
+        }
+    }
+}
